182713d5edb8ef6fdd60771443c5200473c57f95
Mark J. Jensen <mark@jensen.net>
2021-06-02 09:56:09 -0400

    Add the subroutine template to repository.

---
+++ lib/tmpl.f90
@@ -0,0 +1,15 @@
+! Fortran 90 Subroutine Template
+
+!        1         2         3         4         5         6         7         8
+! 3456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789
+
+subroutine tmpl()
+implicit none
+
+! Arguments
+
+
+! Variables
+
+
+end subroutine tmpl
